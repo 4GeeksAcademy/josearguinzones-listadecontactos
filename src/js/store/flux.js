c4055a5f971018c51031e309a6a5303c450fd243
JoseArguinzones <145572262+JoseArguinzones@users.noreply.github.com>
2023-12-14 17:57:30 +0000

    agregar contacto

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -6,6 +6,23 @@ const getState = ({ getStore, setStore }) => {
 		actions: {
 			//(Arrow) Functions that update the Store
 			// Remember to use the scope: scope.state.store & scope.setState()
+			agregarContacto: (full_name, email, address, phone) => {
+				let datos = {
+					full_name: full_name,
+					email: email,
+					agenda_slug: "Jose-arguinzones",
+					address: address,
+					phone: phone
+				};
+				fetch("https://playground.4geeks.com/apis/fake/contact/", {
+					method: "POST",
+					body: JSON.stringify(datos),
+					headers: { "Content-Type": "application/json" }
+				})
+					.then(response => response.json())
+					.then(data => console.log(data))
+					.catch(error => console.log(error));
+			}
 		}
 	};
 };
--- src/js/views/AddContact.js
+++ src/js/views/AddContact.js
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useState, useContext } from "react";
 import { Link } from "react-router-dom";
+import { Context } from "../store/appContext.js";
 
 export const AddContact = () => {
+	const [name, setName] = useState("");
+	const [email, setEmail] = useState("");
+	const [address, setAddress] = useState("");
+	const [phone, setPhone] = useState("");
+
+	const { actions } = useContext(Context);
+
+	function guardar(e) {
+		e.preventDefault();
+		actions.agregarContacto(name, email, address, phone);
+		setName("");
+		setEmail("");
+		setAddress("");
+		setPhone("");
+	}
+
 	return (
 		<div className="container">
 			<div>
@@ -9,21 +26,45 @@ export const AddContact = () => {
 				<form>
 					<div className="form-group">
 						<label>Full Name</label>
-						<input type="text" className="form-control" placeholder="Full Name" />
+						<input
+							type="text"
+							className="form-control"
+							placeholder="Full Name"
+							value={name}
+							onChange={e => setName(e.target.value)}
+						/>
 					</div>
 					<div className="form-group">
 						<label>Email</label>
-						<input type="email" className="form-control" placeholder="Enter email" />
+						<input
+							type="email"
+							className="form-control"
+							placeholder="Enter email "
+							value={email}
+							onChange={e => setEmail(e.target.value)}
+						/>
 					</div>
 					<div className="form-group">
 						<label>Phone</label>
-						<input type="phone" className="form-control" placeholder="Enter phone" />
+						<input
+							type="phone"
+							className="form-control"
+							placeholder="Enter phone"
+							value={phone}
+							onChange={e => setPhone(e.target.value)}
+						/>
 					</div>
 					<div className="form-group">
 						<label>Address</label>
-						<input type="text" className="form-control" placeholder="Enter address" />
+						<input
+							type="text"
+							className="form-control"
+							placeholder="Enter address"
+							value={address}
+							onChange={e => setAddress(e.target.value)}
+						/>
 					</div>
-					<button type="button" className="btn btn-primary form-control">
+					<button type="button" onClick={e => guardar(e)} className="btn btn-primary form-control">
 						save
 					</button>
 					<Link className="mt-3 w-100 text-center" to="/">
